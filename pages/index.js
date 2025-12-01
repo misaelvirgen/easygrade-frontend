0da62cabf9b57dea2f87e7ccf395e06fa6c06b62
Admin <admin@Admins-MacBook-Pro.local>
2025-12-01 14:46:16 -0800

    Initial commit - EasyGrade frontend

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>EasyGrade</title>
+      </Head>
+      <main>
+        <h1>Welcome to EasyGrade</h1>
+        <p>Grading Made Easy.</p>
+      </main>
+    </div>
+  )
+}
